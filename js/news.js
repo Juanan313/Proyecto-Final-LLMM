d646b269c36ecbad2f8e97ad0b68b0b9de6b7f13
Unknown <juanan.pujals@gmail.com>
2018-05-06 19:51:56 +0200

    Comienzo proyecto
    Preparar Readme, ficheros basicos e imports.

---
+++ js/news.js
@@ -0,0 +1,5 @@
+/*--- Función onload para cargar todo los eventos y las llamadas a las funciones -----*/
+window.onload = function () {
+    alert("La página se ha cargado correctamente");
+    console.log("prueba carga jquery: "+$("header h1").text());
+};
