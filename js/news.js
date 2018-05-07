11a7d9c8737c1f790a2446e0987717a216713679
Dual <juanan.pujals@gmail.com>
2018-05-07 18:23:19 +0200

    diseño basico

--- js/news.js
+++ js/news.js
@@ -1,5 +1,5 @@
 /*--- Función onload para cargar todo los eventos y las llamadas a las funciones -----*/
 window.onload = function () {
-    alert("La página se ha cargado correctamente");
+    // alert("La página se ha cargado correctamente");
     console.log("prueba carga jquery: "+$("header h1").text());
 };
