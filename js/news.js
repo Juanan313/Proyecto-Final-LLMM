fc1e7ee54c34e50aa22b40733c7132ff1f27df53
juanan pujals echavarria <juanan.pujals@gmail.com>
2018-05-14 18:26:12 +0200

    primeras pruebas trato json

--- js/news.js
+++ js/news.js
@@ -1,19 +1,34 @@
-/*--- Función onload para cargar todo los eventos y las llamadas a las funciones -----*/
+// Variable constante donde se almacenarán los datos del json
 var NOTICIAS;
 
-
+/*--- Función onload para cargar todo los eventos y las llamadas a las funciones -----*/
 window.onload = function () {
-    // alert("La página se ha cargado correctamente");
+    // prueba de que jquery carga correctamente
     console.log("prueba carga jquery: "+$("header h1").text());
 
+    // Función ajax que carga los datos del json en la variable constante al iniciar la página
     $.ajax({
-        url: 'https://juanan313.github.io/Proyecto-Final-LLMM/src/json/noticias.json',
+        url: 'https://raw.githubusercontent.com/Juanan313/Proyecto-Final-LLMM/master/src/json/noticias.json',
         success: function(respuesta) {
-            console.log(respuesta);
+            NOTICIAS = JSON.parse(respuesta)["Noticias"];
+            console.log("Se han obtenido los datos");
+            
         },
         error: function() {
             console.log("No se ha podido obtener la información");
         }
     });
+
+
 };
+
+/* ---- TRATAR DATOS JSON ----*/
+
+prepararEntradillas() {
+
+    $(NOTICIAS).each({
+
+    });
+
+}
 
