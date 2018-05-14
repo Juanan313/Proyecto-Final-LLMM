3e24152860211c017bd833ebbd83cc6b84540d93
juanan pujals echavarria <juanan.pujals@gmail.com>
2018-05-14 17:26:08 +0200

    1st try ajax

--- js/news.js
+++ js/news.js
@@ -1,5 +1,19 @@
 /*--- Función onload para cargar todo los eventos y las llamadas a las funciones -----*/
+var NOTICIAS;
+
+
 window.onload = function () {
     // alert("La página se ha cargado correctamente");
     console.log("prueba carga jquery: "+$("header h1").text());
+
+    $.ajax({
+        url: 'https://juanan313.github.io/Proyecto-Final-LLMM/src/json/noticias.json',
+        success: function(respuesta) {
+            console.log(respuesta);
+        },
+        error: function() {
+            console.log("No se ha podido obtener la información");
+        }
+    });
 };
+
