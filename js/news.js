f35c8b8481b302d7059bab02f5048791762181fa
Unknown <juanan.pujals@gmail.com>
2018-05-28 22:22:42 +0200

    añadiendo elementos alas entradillas

--- js/news.js
+++ js/news.js
@@ -19,12 +19,16 @@ window.onload = function () {
         }
     });
 
-    jQuery(function () {
-        var page = location.pathname.split('/').pop();
-        alert(page)
-        $('#nav li a[href="' + page + '"]').addClass('linkActivo')
-        console.log($('#nav li a[href="' + page + '"]'))
-    })
+    // $.getJSON("../src/noticias.json", function (data) {
+    //     NOTICIAS = data;
+    // });
+
+    // jQuery(function () {
+    //     var page = location.pathname.split('/').pop();
+    //     alert(page)
+    //     $('#nav li a[href="' + page + '"]').addClass('linkActivo')
+    //     console.log($('#nav li a[href="' + page + '"]'))
+    // })
 
     $("#pobre").on("click", function () {
         prepararEntradillas();
@@ -57,13 +61,19 @@ function prepararEntradilla(noticia) {
 
     var entradilla = $("<section></section>");
     
-    var titulo = $("<h1></h1>").addClass("tituloNoticia").append(noticia.Titulo);
+    var titulo = $("</h2>").addClass("tituloNoticia").append(noticia.Titulo);
     titulo.appendTo(entradilla);
+
+    var postTitulo = $("</h3>").append(noticia.PostTiutlo);
+    postTitulo.appendTo(entradilla);
+
+    var fecha = $("</div>").addClass("fechaNoticia").append(noticia.Fecha);
+    fecha.appendTo(entradilla);
     
-    var imagen = "<img src='"+noticia.Imagen+"' alt='"+noticia.Titulo+"'>";
+    var imagen = "<img src='"+noticia.Imagen+"' alt='"+noticia.Titulo+"' class='articleImg'>";
     entradilla.append(imagen);
     
-    var textoEntradilla = $("<p></p>").append(noticia.Entradilla);
+    var textoEntradilla = $("</p>").append(noticia.Entradilla);
     textoEntradilla.appendTo(entradilla);
 
     $("main").append(entradilla);
